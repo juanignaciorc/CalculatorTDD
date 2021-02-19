dbdde95c694328c601fed270764c2290d05f2e35
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2021-02-19 03:52:25 -0300

    Readme update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const calculatorElement = screen.getByText(/calculator/i);
+  expect(calculatorElement).toBeInTheDocument();
 });
